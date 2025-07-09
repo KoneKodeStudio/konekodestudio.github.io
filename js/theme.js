a1da1b4091eb15bc40eef8cf672fb66dd41483c4
KoneKode <konekode.studio@gmail.com>
2025-07-09 08:48:22 +0200

    Total refactoring
    BUG fixes:
      - fixed cursor affected by padding (view the exalidraw sketch by SonoNessunk)
    
    CHANGES:
      - changed the "navbar"(header) from grid to flex
      - changed temporarly the background color, only for visual debugging
      - changed css selector names. e.g. search -> nav-search-field
    
    ADD:
      - added search icon
      - added theme toggle function
      - downloaded svg icons in case it needed.
    
    REMOVES:
      - temporarly removed the drawer, only for visual debugging

---
+++ js/theme.js
@@ -0,0 +1,21 @@
+const body = document.querySelector("body"),
+  modeToggle = document.querySelector(".nav-theme-toggle");
+
+let getMode = localStorage.getItem("mode");
+if (getMode === "light-mode") {
+  body.classList.add("light");
+  modeToggle.classList.add("active");
+}
+
+// js code to toggle dark and light mode
+modeToggle.addEventListener("click", () => {
+  modeToggle.classList.toggle("active");
+  body.classList.toggle("light");
+
+  // js code to keep user selected mode even page refresh or file reopen
+  if (!body.classList.contains("light")) {
+    localStorage.setItem("mode", "dark-mode");
+  } else {
+    localStorage.setItem("mode", "light-mode");
+  }
+});
